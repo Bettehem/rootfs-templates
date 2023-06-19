3946794c286b702951f99803fee17cc1a9a26244
Bettehem <chris.mustola@gmail.com>
2023-06-20 00:36:59 +0300

    Added copying kernel and stuff to genzip.sh
    Added the "Copy kerneld and stuff" section from genimage.sh to genzip.sh

--- scripts/genzip.sh
+++ scripts/genzip.sh
@@ -2,19 +2,20 @@
 ROOTFS_PATH=$(find . -maxdepth 1 -mindepth 1 -type d -name .debos-*)/root
 ROOTFS_SIZE=$(du -sm $ROOTFS_PATH | awk '{ print $1 }')
 
-ZIP_NAME=$1
+ZIP_NAME=${1}
+WORK_DIR="android-recovery-flashing-template"
 IMG_SIZE=$(( ${ROOTFS_SIZE} + 250 )) # FIXME 250MB contingency
 IMG_MOUNTPOINT=".image"
 
 # create root img
 echo "Creating an empty root image"
-dd if=/dev/zero of=rootfs.img bs=1M count=${IMG_SIZE}
-mkfs.ext4 -O ^metadata_csum -O ^64bit -F rootfs.img
+dd if=/dev/zero of="${WORK_DIR}/data/rootfs.img" bs=1M count=${IMG_SIZE}
+mkfs.ext4 -O ^metadata_csum -O ^64bit -F "${WORK_DIR}/data/rootfs.img"
 
 # mount the image
 echo "Mounting root image"
 mkdir -p $IMG_MOUNTPOINT
-mount -o loop rootfs.img ${IMG_MOUNTPOINT}
+mount -o loop "${WORK_DIR}/data/rootfs.img" ${IMG_MOUNTPOINT}
 
 # copy rootfs content
 echo "Syncing rootfs content"
@@ -26,9 +27,19 @@ sync
 echo "umount root image"
 umount $IMG_MOUNTPOINT
 
+# Copy kernel and stuff
+bootimage=$(find "${ROOTFS_PATH}/boot" -iname boot.img* -type f | head -n 1)
+recovery=$(find "${ROOTFS_PATH}/boot" -iname recovery.img* -type f | head -n 1)
+dtbo=$(find "${ROOTFS_PATH}/boot" -iname dtbo.img* -type f | head -n 1)
+vbmeta=$(find "${ROOTFS_PATH}/boot" -iname vbmeta.img* -type f | head -n 1)
+
+[ -e "${bootimage}" ] && cp "${bootimage}" ${WORK_DIR}/data/boot.img
+[ -e "${recovery}" ] && cp "${recovery}" ${WORK_DIR}/data/recovery.img
+[ -e "${dtbo}" ] && cp "${dtbo}" ${WORK_DIR}/data/dtbo.img
+[ -e "${vbmeta}" ] && cp "${vbmeta}" ${WORK_DIR}/data/vbmeta.img
+
 # generate flashable zip
 echo "Generating recovery flashable zip"
-mv rootfs.img android-recovery-flashing-template/data/rootfs.img
-(cd android-recovery-flashing-template ; zip -r9 ../out/$ZIP_NAME * -x .git README.md *placeholder)
+(cd ${WORK_DIR}; zip -r9 ../out/$ZIP_NAME * -x .git README.md *placeholder)
 
 echo "done."
